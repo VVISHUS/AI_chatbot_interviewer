fc97dbb8636b6ebb009674e6469c4211e9e876d4
VAIBHAV SINGH <115949686+VVISHUS@users.noreply.github.com>
2025-06-10 07:53:13 +0530

    Update get_JDs.py

--- utils/get_JDs.py
+++ utils/get_JDs.py
@@ -4,14 +4,25 @@ import glob
 def get_jd_options():
     """Extract job positions from JD filenames"""
     jd_dict = {}
-    jd_files = glob.glob("G:/scripts/PG-AGI/hiring-assistant-chatbot/JDs/*")
-    
+
+    # Get the path to the current script (e.g., utils/)
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Move up one level to the main app folder
+    app_dir = os.path.dirname(base_dir)
+
+    # Construct the path to the JDs folder
+    jd_folder = os.path.join(app_dir, "JDs")
+
+    # Get all files inside JDs
+    jd_files = glob.glob(os.path.join(jd_folder, "*"))
+
     for file_path in jd_files:
         filename = os.path.basename(file_path)
         # Extract part before '_talenscoutJD'
-        if 'talenscout' or 'JD' in filename:
+        if 'talenscout' in filename or 'JD' in filename:
             keys = filename.split('_')
-            key="".join(keys[:-2])
+            key = "".join(keys[:-2]) if len(keys) >= 2 else filename
             jd_dict[key] = file_path
         else:
             # If no '_talenscoutJD' pattern, use filename without extension
@@ -19,8 +30,3 @@ def get_jd_options():
             jd_dict[key] = file_path
     
     return jd_dict
-
-
-
-# JDs = get_jd_options()
-# print(JDs.keys())
